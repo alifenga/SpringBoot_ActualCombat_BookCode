86cda465a81c07c64038e8d8680df413c22a57b7
lifeng <alifenga@163.com>
2019-06-20 22:20:33 +0800

    新增boot ssl案例

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # SpringBoot_ActualCombat_BookCode
+## 本库代码案例均来自《SpringBoot实战》书籍
--- spring_boot_web/src/main/java/xyz/alifenga/SpringBootWebApplication.java
+++ spring_boot_web/src/main/java/xyz/alifenga/SpringBootWebApplication.java
@@ -2,12 +2,33 @@ package xyz.alifenga;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import xyz.alifenga.bean.Person;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
+@Controller
 public class SpringBootWebApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootWebApplication.class, args);
     }
 
+
+    @RequestMapping("/")
+    public String index(Model model){
+        Person person = new Person("张三",11);
+
+        List<Person> list = new ArrayList<>();
+        list.add(new Person("李四",12));
+        list.add(new Person("王五",13));
+        list.add(new Person("赵六",14));
+        model.addAttribute("singlePerson",person);
+        model.addAttribute("people",list);
+        return "index";
+    }
 }
